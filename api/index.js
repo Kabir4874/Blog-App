bb4a9276ed1a83d036cc8834c46de18f0ecb2e95
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-08-25 21:41:07 +0600

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+app.use("/api/user", userRoutes);
