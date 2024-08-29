085862defbc4f97ffe422d666d83d215fafdda64
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-08-29 15:01:19 +0600

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,51 @@
+const generateToken = require("../middlewares/generateToken");
 const User = require("../models/user.model");
 class authController {
   register = async (req, res) => {
     try {
       const { email, password, username } = req.body;
       const user = new User({ email, password, username });
-      console.log(user);
+      await user.save();
+      res.status(200).send({ message: "User Registered successfully", user });
     } catch (error) {
       console.error("Error in register user: ", error);
       res.status(500).send({ message: "Error in register user" });
     }
   };
+  login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch) {
+        return res.status(401).send({ message: "Wrong password" });
+      }
+      const token = await generateToken(user._id);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: true,
+      });
+      res.status(200).send({
+        message: "Login successfully",
+        token,
+        user: {
+          id: user._id,
+          email: user.email,
+          username: user.username,
+          role: user.role,
+        },
+      });
+    } catch (error) {
+      console.error("Error in login user: ", error);
+      res.status(500).send({ message: "Error in login user" });
+    }
+  };
+  logout= async(req,res)=>{
+    console.log('logout');
+  }
 }
 module.exports = new authController();
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post("/register",authController.register );
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 module.exports = router;
