8956d33dc0cf8af1d8e6020e487235be720aca05
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-08-29 23:59:56 +0600

    single blog

--- frontend/src/components/blogs/SingleBlogCard.jsx
+++ frontend/src/components/blogs/SingleBlogCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import formatDate from "../../utils/formatDate";
+import EditorJSHTML from "editorjs-html";
+const editorJSHTML = EditorJSHTML();
 
 const SingleBlogCard = ({ blog }) => {
   const {
@@ -13,6 +15,8 @@ const SingleBlogCard = ({ blog }) => {
     createdAt,
   } = blog || {};
 
+  //   const htmlContent = editorJSHTML.parse(content).join("");
+
   return (
     <div className="bg-white p-8">
       <div>
@@ -23,10 +27,18 @@ const SingleBlogCard = ({ blog }) => {
         </p>
       </div>
       <div>
-        <img src={coverImg} alt="cover img" className="w-full md:h-[520px] bg-cover"/>
+        <img
+          src={coverImg}
+          alt="cover img"
+          className="w-full md:h-[520px] bg-cover"
+        />
       </div>
-      <div>
-        
+      <div className="mt-8 space-y-4">
+        {/* <div dangerouslySetInnerHTML={{ __html: htmlContent }} className=" space-y-3 editorjsdiv"/> */}
+        <div>
+          <span className="text-lg font-medium mr-2">Rating:</span>
+          <span>{rating} (based on 2,370 reviews)</span>
+        </div>
       </div>
     </div>
   );
