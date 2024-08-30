4a30ea3ecc706bd3199f59c1effed11f3592aafb
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-08-30 17:55:54 +0600

    login logout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,10 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import store from "./redux/store.js";
 import Router from "./routes/Router.jsx";
-
+import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={Router} />
+    <Toaster position="top-center" reverseOrder={false} />
   </Provider>
 );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useLoginUserMutation } from "../redux/features/auth/authApi";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [loginUser, { isLoading }] = useLoginUserMutation();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    try {
+      const response = await loginUser(data).unwrap();
+      const { token, user } = response;
+      toast.success("Login Successful");
+      navigate("/");
+    } catch (error) {
+      setMessage("Please provide a valid email and password");
+    }
+  };
   return (
     <div className="max-w-sm bg-white mx-auto p-8 mt-36">
       <h2 className="text-2xl font-semibold pt-5">Please Login</h2>
-      <form className="space-y-5 max-w-sm  mx-auto pt-8">
+      <form onSubmit={handleLogin} className="space-y-5 max-w-sm  mx-auto pt-8">
         <input
           type="email"
           value={email}
           placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
           required
           className="w-full bg-bgPrimary focus:outline-none px-5 py-3"
         />
@@ -20,11 +40,15 @@ const Login = () => {
           type="password"
           value={password}
           placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
           required
           className="w-full bg-bgPrimary focus:outline-none px-5 py-3"
         />
         {message && <p className="text-red-500">{message}</p>}
-        <button className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium rounded-md px-5 py-3 transition-all duration-300">
+        <button
+          disabled={isLoading}
+          className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium rounded-md px-5 py-3 transition-all duration-300"
+        >
           Login
         </button>
       </form>
